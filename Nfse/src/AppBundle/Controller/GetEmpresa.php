9692c8f46e3b9f7cf80f9ae1c9899e99bf0a3d2e
Vinicius <vinicius@sibrax.com.br>
2016-09-06 17:39:39 -0300

    .

---
+++ Nfse/src/AppBundle/Controller/GetEmpresa.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use BeSimple;
+
+/**
+ * WSLondrina controller.
+ *
+ */
+class GetEmpresa extends Controller
+{
+
+    public function getIdEmpresa()
+    {
+          return 1;
+    }
+
+
+}
